650b93bee348d0b65dbec55bacbec7bb6bb3dab8
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-09-25 19:12:17 +0300

    form old school

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import {useRef} from "react";
+
 const App = () => {
+    const name = useRef();
+    const age = useRef();
+
+    const submit = (e) => {
+        e.preventDefault()
+        const data = {name: name.current.value, age: age.current.value}
+        console.log(data);
+    };
+
     return (
         <div>
-            App
+            <form onSubmit={submit}>
+                <input type="text" placeholder={'name'} ref={name}/>
+                <input type="text" placeholder={'age'} ref={age}/>
+                <button>save</button>
+            </form>
         </div>
     );
 };
