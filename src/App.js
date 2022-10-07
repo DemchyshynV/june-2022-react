da49d55851117b6f5290bcad5eadd267cf2bb1d2
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-10-07 20:21:21 +0300

    sdfsf

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import {useForm} from "react-hook-form";
+import axios from "axios";
+import {useEffect, useState} from "react";
+
 const App = () => {
+    const {register, handleSubmit} = useForm({defaultValues:{id:1}});
+    const [users, setUsers] = useState([]);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users').then(({data}) => setUsers(data))
+    }, [])
+
+    const submit = async ({id}) => {
+        const {data} = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+        setUser(data)
+    };
+
     return (
         <div>
-            App
+            <form onChange={handleSubmit(submit)}>
+                <select {...register('id')}>
+                    {users.map(user => (
+                        <option key={user.id} value={user.id}>{user.name}</option>
+                    ))}
+                </select>
+                {/*<button>getInfo</button>*/}
+            </form>
+            {user && <div>{JSON.stringify(user)}</div>}
         </div>
     );
 };
